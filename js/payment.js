c31878b2116209c0569dc22441597ca99a9d4288
JaeguSon <maruch3088@gmail.com>
2020-06-25 11:22:26 +0900

    asdf

--- js/payment.js
+++ js/payment.js
@@ -4,7 +4,7 @@ function payment() {
         application_id: "5ed87b408f0751001efcd31d",
         name: '아트바이어스 스튜디오&클래스', //결제창에서 보여질 이름
         pg: 'payapp',
-        method: 'npay', //결제수단, 입력하지 않으면 결제수단 선택부터 화면이 시작합니다.
+        //method: '', //결제수단, 입력하지 않으면 결제수단 선택부터 화면이 시작합니다.
         show_agree_window: 0, // 부트페이 정보 동의 창 보이기 여부
         items: [
             {
