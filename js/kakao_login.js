d658c55c06d321165189a630c00822763c85a220
JaeguSon <maruch3088@gmail.com>
2020-07-02 22:15:40 +0900

    asdf

--- js/kakao_login.js
+++ js/kakao_login.js
@@ -2,11 +2,7 @@ var access_token;
 var refresh_token;
 var email;
 
-// SDK를 초기화 합니다. 사용할 앱의 JavaScript 키를 설정해 주세요.
-Kakao.init('d88bea310f4a39afbea3146fe1c0fa72');
 
-// SDK 초기화 여부를 판단합니다.
-console.log(Kakao.isInitialized());
 
 $("#kakao_login").on("click", function() {
 
