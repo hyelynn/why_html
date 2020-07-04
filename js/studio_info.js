c905703a8655eb2b6bf53c84cadb3da3511f761e
JaeguSon <maruch3088@gmail.com>
2020-07-04 14:46:53 +0900

    200704

--- js/studio_info.js
+++ js/studio_info.js
@@ -19,16 +19,29 @@ var url = location.href;
                         var result = JSON.parse(xhr.responseText);
                         console.log(result[0]);
                         
+                        for (var img in result[0].length) {
+                            $("indicators").append()
+                            "<li data-target='#carouselStudioView' data-slide-to='"+img+"' class='active'></li>"
+
+                            $("#info").append(
+                            $("<div class='carousel-item active'>"+
+                            "<img src="+result[img].studio_images+" class='d-block' w-100 alt='...'></div>"));
+                        }
+                        
+
                         $("#info").append(
                         $("<div class='my-5'>"+
                         "<h4 class='mb-4 text-bullet'>소개</h4>"+
-                        "<p class='font-weight-light font-size-s'>"+result+"</p></div>"+
+                        "<p class='font-weight-light font-size-s'>"+result[0].studio_info+"</p></div>"+
                         "<div class='my-5'>"+
                         "<h4 class='mb-4 text-bullet'>편의시설</h4>"+
                         "<div class='d-flex-fill font-weight-light font-size-s'>"+
-                        "<span class='mr-2'>"+result+"</span></div></div>"));
+                        "<span class='mr-2'>"+result[0].studio_useful+"</span></div></div>"));
                     } else {
-
+                        if (!regexp.test(valnum)) {
+                            alert('접속 오류'); 
+                            location.href = 'studio.html';  //잘못된 접속 시 페이지 강제 이동
+                        }
                     }
                 }
                 else{
