fd594133ec34177137e78aba5d4cb8f8921eab09
JaeguSon <maruch3088@gmail.com>
2020-07-05 23:20:07 +0900

    images

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
     var items = sessionStorage.getItem("cart");
-   
+    console.log(items)
+
+    $.each(data, function(index, item){
+        $('#cart').$append(
+            $("<li class='list-group-item pr-4 pl-3 py-4'>"+
+            "<div class='d-lg-flex align-items-center custom-inputs'>"+
+            "<div class='d-flex align-items-lg-center'>"+
+            "<input type='checkbox' class='custom-checkbox mr-sm-3 mr-2'>"+
+            "<div class='d-flex justify-content-center overflow-hidden mr-sm-3 mr-2 cart-thumbs'>"+
+            "<img src='"+item.img+"' class='align-self-center w-100 h-100' alt=''></div>"+
+            "<div><span class='d-block font-weight-light text-red cart-item-category'>연습실</span>"+
+            "<a href='/why_html/sub/studio_view.html' class='text-decoration-none'>"+
+            "<h4 class='text-gray cart-item-title'>"+item.title+"</h4></a>"+
+            "<p class='font-weight-light text-gray mb-0'>"+item.date+"</p>"+
+            "<p class='font-weight-light text-light-gray mb-0'>추가인원<span class='ml-5'>+10,000</span></p>"+
+            "<div class='text-right ml-auto'>"+
+            "<p class='font-weight-light text-gray mb-0'>합계</p>"+
+            "<h2 class='mb-0'>" + item.price + "원</h2><p class='font-weight-light text-light-gray mb-0'>(VAT포함)</p></div></div></li>")
+        );
+    })
 })
 
 
 
 
-"<li class='list-group-item pr-4 pl-3 py-4'>"+
-"<div class='d-lg-flex align-items-center custom-inputs'>"+
-"<div class='d-flex align-items-lg-center'>"+
-"<input type='checkbox' class='custom-checkbox mr-sm-3 mr-2'>"+
-"<div class='d-flex justify-content-center overflow-hidden mr-sm-3 mr-2 cart-thumbs'>"+
-"<img src='/why_html/img/cart-1.jpg' class='align-self-center w-100 h-100' alt=''></div>"+
-"<div><span class='d-block font-weight-light text-red cart-item-category'>연습실</span>"+
-"<a href='/why_html/sub/studio_view.html' class='text-decoration-none'>"+
-"<h4 class='text-gray cart-item-title'>사당역 제이그라운드 안무연습실</h4></a>"+
-"<p class='font-weight-light text-gray mb-0'>예약일시 : 2020-05-28 13 : 05 ~ 2020-05-29 15 : 30</p>"+
-"<p class='font-weight-light text-light-gray mb-0'>추가인원<span class='ml-5'>+10,000</span></p>"+
-"<div class='text-right ml-auto'>"+
-"<p class='font-weight-light text-gray mb-0'>합계</p>"+
-"<h2 class='mb-0'>" + 65000 + "원</h2><p class='font-weight-light text-light-gray mb-0'>(VAT포함)</p></div></div></li>";
+
