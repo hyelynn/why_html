bb3e49ef4ae8791614d5482f2acc0a9f43ecc5c2
JaeguSon <maruch3088@gmail.com>
2020-07-09 10:10:21 +0900

    index

--- js/header.js
+++ js/header.js
@@ -6,10 +6,39 @@ function login_header() {
 
     if (navigator.platform) {
       var append_html = "";
-      if (false) {
-        console.log('PC');
+      if (0 >= filter.indexOf(navigator.platform.toLowerCase())) {
         append_html =
-        "<ul class='navbar-nav border-top pt-3 m-lg-auto mt-2 font-size-xs d-block d-lg-none'>" +
+        "<ul class='navbar-nav ml-auto align-items-center d-none d-lg-flex'>" +
+        "<li class='nav-item mr-2 font-size-md'>" +
+        "<a class='nav-link text-dark' href='/member/calendar.html'><i class='far fa-calendar-alt'></i></a></li>" +
+        "<li class='nav-item mr-2 font-size-md'>" +
+        "<a class='nav-link text-dark' href='/member/recent_list.html'><i class='far fa-clock'></i></a></li>" +
+        "<li class='nav-item mr-3 font-size-md'>" +
+        "<a class='nav-link text-dark' href='/member/cart.html'><i class='fas fa-shopping-cart'></i></a></li>" +
+        "<li class='nav-item'><div class='dropdown'>" +
+        "<button class='btn border text-dark dropdown-toggle bg-white rounded-xl' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>" +
+        "<div class='d-flex'>" +
+        "<div class='position-relative mr-2 nav-user-profile'>" +
+        "<span class='position-absolute rounded-pill bg-hot-pink nav-user-notify'></span>" +
+        "<img src='/img/profile-photo@2x.jpg' class='rounded-circle nav-user-profile-img'></div>" +
+        "<span>" +
+        nickname +
+        "</span></div></button>" +
+        "<div class='dropdown-menu dropdown-menu-lg-right rounded-xl' aria-labelledby='dropdownMenuButton'>" +
+        "<a class='dropdown-item' href='#x'>예약 리스트</a>" +
+        "<a class='dropdown-item' href='#x'>찜 리스트</a>" +
+        "<a class='dropdown-item' href='#x'>나의 이용 후기</a>" +
+        "<a class='dropdown-item' href='#x'>채팅</a>" +
+        "<a class='dropdown-item' href='#x'>1:1 문의<span class='ml-2 d-inline-block rounded-pill align-top mt-1 bg-hot-pink nav-notify'></span></a>" +
+        "<div class='dropdown-divider'></div>" +
+        "<a class='dropdown-item' href='#x'>회원 정보 관리</a>" +
+        "<a class='dropdown-item' href='#x'>호스트 전환</a>" +
+        "<a class='dropdown-item text-hot-pink' href='#x' onclick='onLogout();'>로그아웃</a></div></div></li></ul>";
+        
+      } else {
+        console.log('MOBILE');
+        append_html =
+          "<ul class='navbar-nav border-top pt-3 m-lg-auto mt-2 font-size-xs d-block d-lg-none'>" +
         "<li class='nav-item mx-lg-3 mx-2 mb-2'>" +
         "<div class='d-flex'><div class='position-relative mr-2 nav-user-profile'>" +
         "<span class='position-absolute rounded-pill bg-hot-pink nav-user-notify'></span>" +
@@ -24,35 +53,6 @@ function login_header() {
         "<li class='nav-item mx-lg-3 mx-2'><a class='nav-link' href='#x'>회원 정보 관리</a></li>" +
         "<li class='nav-item mx-lg-3 mx-2'><a class='nav-link' href='#x'>호스트 전환</a></li>" +
         "<li class='nav-item mx-lg-3 mx-2'></li><a class='nav-link text-hot-pink' href='#x'>로그아웃</a></li></ul>";
-      } else {
-        console.log('MOBILE');
-        append_html =
-          "<ul class='navbar-nav ml-auto align-items-center d-none d-lg-flex'>" +
-          "<li class='nav-item mr-2 font-size-md'>" +
-          "<a class='nav-link text-dark' href='/member/calendar.html'><i class='far fa-calendar-alt'></i></a></li>" +
-          "<li class='nav-item mr-2 font-size-md'>" +
-          "<a class='nav-link text-dark' href='/member/recent_list.html'><i class='far fa-clock'></i></a></li>" +
-          "<li class='nav-item mr-3 font-size-md'>" +
-          "<a class='nav-link text-dark' href='/member/cart.html'><i class='fas fa-shopping-cart'></i></a></li>" +
-          "<li class='nav-item'><div class='dropdown'>" +
-          "<button class='btn border text-dark dropdown-toggle bg-white rounded-xl' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>" +
-          "<div class='d-flex'>" +
-          "<div class='position-relative mr-2 nav-user-profile'>" +
-          "<span class='position-absolute rounded-pill bg-hot-pink nav-user-notify'></span>" +
-          "<img src='/img/profile-photo@2x.jpg' class='rounded-circle nav-user-profile-img'></div>" +
-          "<span>" +
-          nickname +
-          "</span></div></button>" +
-          "<div class='dropdown-menu dropdown-menu-lg-right rounded-xl' aria-labelledby='dropdownMenuButton'>" +
-          "<a class='dropdown-item' href='#x'>예약 리스트</a>" +
-          "<a class='dropdown-item' href='#x'>찜 리스트</a>" +
-          "<a class='dropdown-item' href='#x'>나의 이용 후기</a>" +
-          "<a class='dropdown-item' href='#x'>채팅</a>" +
-          "<a class='dropdown-item' href='#x'>1:1 문의<span class='ml-2 d-inline-block rounded-pill align-top mt-1 bg-hot-pink nav-notify'></span></a>" +
-          "<div class='dropdown-divider'></div>" +
-          "<a class='dropdown-item' href='#x'>회원 정보 관리</a>" +
-          "<a class='dropdown-item' href='#x'>호스트 전환</a>" +
-          "<a class='dropdown-item text-hot-pink' href='#x' onclick='onLogout();'>로그아웃</a></div></div></li></ul>";
       }
     }
     $("#navbarNavDropdown").empty();
