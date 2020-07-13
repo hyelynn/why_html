39614773b5f7040670dabcbf4caf03631ffd2502
JaeguSon <maruch3088@gmail.com>
2020-07-13 11:25:46 +0900

    qareview

--- js/studio_info.js
+++ js/studio_info.js
@@ -217,9 +217,23 @@ function price_maker() {
 function sendQna() {
   let id = sessionStorage.getItem("id");
   if (id != null) {
+    console.id('asdf');
     let msg = $("#qna_text").val();
 
     var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200 || xhr.status === 201) {
+          var result = JSON.parse(xhr.responseText);
+          
+        } else {
+          alert("접속 오류");
+          location.href = "studio.html"; //잘못된 접속 시 페이지 강제 이동
+        }
+      } else {
+      }
+    };
+
     xhr.open("POST", "http://3.34.150.116:3000/studio/qna");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send("studio_key=" + value + "&user_id=" + id + "&msg=" + msg);
@@ -231,6 +245,7 @@ function sendQna() {
 function sendReview() {
   let id = sessionStorage.getItem("id");
   if (id != null) {
+    console.id('asdf');
     let msg = $("#review_text").val();
 
     var xhr = new XMLHttpRequest();
