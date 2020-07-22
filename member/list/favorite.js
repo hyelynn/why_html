da87f836c411e4b591d5014f29d51ae04cb0871a
JaeguSon <maruch3088@gmail.com>
2020-07-22 09:07:13 +0900

    fav

--- member/list/favorite.js
+++ member/list/favorite.js
@@ -12,32 +12,34 @@ $(document).ready(function () {
             $("#favorite_list").append(
               $(
                 "<div class='col-lg-3 col-6 mb-lg-5 mb-4'>" +
-                  "<div id = '" + result[0].obj_category +"_index' style='display: none;''>" +
-                  result[0].obj_key +
+                  "<div id = '" + result[i].obj_category +"_index' style='display: none;''>" +
+                  result[i].obj_key +
                   "</div>" +
                   "<div class='card border-0 bg-transparent'>" +
                   "<div class='position-absolute p-md-2 p-1 w-100 z-index-2'>" +
                   "<div class='p-lg-2 p-1 d-flex list-tag list-tag-yellow'>" +
                   "<span class='align-self-center list-tag-title'>바로결제</span>" +
                   "</div>" +
-                  "<button type='button' class='float-right bg-transparent border-0 p-0 text-white font-size-md' onclick='addWishList(this);'><i class='far fa-heart'></i></button>" +
+                  "<button type='button' class='float-right bg-transparent border-0 p-0 text-white font-size-md' onclick='addWishList(this);'><i class='far fa-heart text-red'></i></button>" +
+                  "<div id = 'key' style='display: none;''>" +
+                  result[i].obj_key +"</div>"+
                   "</div>" +
                   "<img src='" +
-                  result[0].obj_thumbnail +
+                  result[i].obj_thumbnail +
                   "' class='card-img-top rounded-0'>" +
                   "<h5 class='mt-lg-3 mt-2'>" +
-                  result[0].obj_name +
+                  result[i].obj_name +
                   "</h5>" +
                   "<p class='font-weight-light border-bottom pb-2 mb-2 font-size-xs'>" +
-                  result[0].obj_subname +
+                  result[i].obj_subname +
                   "</p>" +
-                  "<a href='"+ result[0].obj_category +"_view.html?" +
-                  result[0].obj_key +
+                  "<a href='"+ result[i].obj_category +"_view.html?" +
+                  result[i].obj_key +
                   "' class='stretched-link text-decoration-none'>" +
                   "<span class='text-red font-weight-bold font-size-l'>" +
-                  result[0].obj_rp +
+                  result[i].obj_rp +
                   "</span><span class='text-dark font-weight-light pl-1 font-size-s'>원/" +
-                  result[0].obj_price_type +
+                  result[i].obj_price_type +
                   "</span>" +
                   "</a> </div></div>"
               )
@@ -56,4 +58,34 @@ $(document).ready(function () {
 });
 
 
+// wishlist btn
+function addWishList(obj) {
+  let uid = sessionStorage.getItem("id");
+  if (uid == null) {
+    alert("로그인이 필요합니다");
+    location.reload();
+  } else {
+    let icon = $(obj).find("i");
+    if (icon.hasClass("far fa-heart")) {
+      $(icon).attr("class", "fas fa-heart text-red");
+    } else {
+      $(icon).attr("class", "far fa-heart");
+    }
 
+    var value = $(obj).find("#key").text();
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200 || xhr.status === 201) {
+          console.log("조아여");
+          location.reload();
+        }
+      }
+    };
+
+    xhr.open("POST", "http://3.34.150.116:3000/object/like");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("key=" + value + "&ukey=" + uid);
+  }
+}
