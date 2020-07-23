c9c81696d1a5e32070d7ba37b8cac0c73441c8cc
JaeguSon <maruch3088@gmail.com>
2020-07-23 19:24:09 +0900

    main

--- main/perform.js
+++ main/perform.js
@@ -8,24 +8,10 @@ function initPerform() {
       //성공 시
       success: function (data) {
         $.each(data, function (_, item) {
-          var prefix = "";
-          if (data == null) {
-            prefix =
-              "<div class='position-absolute p-md-2 p-1 w-100 z-index-2'>" +
-              "<div class='p-lg-2 p-1 d-flex list-tag list-tag-sky'>" +
-              "<span class='align-self-center list-tag-title'>예약결제</span></div>" +
-              "<button type='button' class='float-right bg-transparent border-0 p-0 text-white font-size-md' onclick='addWishList(this);'><i class='far fa-heart'></i></button></div>";
-          } else {
-            prefix =
-              "<div class='position-absolute p-md-2 p-1 w-100 z-index-2'>" +
-              "<div class='p-lg-2 p-1 d-flex list-tag list-tag-yellow'>" +
-              "<span class='align-self-center list-tag-title'>즉시결제</span></div>" +
-              "<button type='button' class='float-right bg-transparent border-0 p-0 text-white font-size-md' onclick='addWishList(this);'><i class='far fa-heart'></i></button></div>";
-          }
+          
           $("#main-audition-slide").append(
             $(
                 "<div class='col-lg-3 col-6'><div class='card border-0'>" +
-                prefix +
                 "<img src='"+item.obj_thumbnail+"' class='card-img-top rounded-0'>" +
                 "<h5 class='mt-lg-3 mt-2'>" +
                 item.obj_name +
