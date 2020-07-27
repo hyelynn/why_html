c2658d3b2b93484196823783b4c0843e0af520f1
JaeguSon <maruch3088@gmail.com>
2020-07-27 15:26:46 +0900

    afw

--- member/list/qna.js
+++ member/list/qna.js
@@ -9,28 +9,32 @@ $(document).ready(function () {
           if (xhr.status === 200 || xhr.status === 201) {
             var result = JSON.parse(xhr.responseText);
             for (var i = 0; i < result.length; i++) {
-                $("#reserv_item").append($(
-                    "<li class='list-group-item p-3 mb-3 border'><div class='row align-items-center'><div class='col'><div class='d-flex align-items-lg-center'>"+
-                    "<div class='d-flex justify-content-center align-items-center overflow-hidden mr-4 item-thumbs'><img src='"+result[i].reserv_img+"' alt=''></div>"+
-                    "<div class='item-texts font-weight-light'><span class='text-red'>예약번호 : "+ result[i].reserv_id +"</span>" +
-                    "<a href='/sub/class_view.html' class='text-decoration-none'><h4 class='text-gray'>"+result[i].reserv_name+"</h4></a>"+
-                    "<ul class='list-unstyled text-light-gray mb-2'><li>예약호실 : "+result[i].reserv_place+"</li><li>예약자명 : "+result[i].reserv_pers+"</li><li>예약일시 : "+result[i].reserv_time+"</li></ul>"+
-                    "<a href='chat_user_view.html?"+result[i].reserv_host+"' class='d-inline-block text-decoration-none text-sky mr-2'>1:1 대화하기</a>"+
-                    "<a type='button' data-toggle='modal' data-target='#notifyModal' class='d-inline-block text-decoration-none text-sky'>전화하기</a></div></div></div></div></li>"    
-                ))
-            }
+              var review = "<div class='accordion host-accordion text-gray mb-lg-5 mb-3' id='qnaItem1'>"
+              "<div class='card'><div class='card-header d-flex justify-content-between align-items-center' id='itemHeading1'>"
+              "<div class='mr-lg-5 mr-3'><p class='my-2'>"+ result[i].qna_msg + +"</p>"+
+              "<span class='state-text bottom'>"+result[i].obj_name+"<span class='ml-md-4 d-md-inline-block d-block'>"+result[i].qna_date+"</span></span></div>" +
+              "<button class='btn accordion-btn' type='button' data-toggle='collapse' data-target='#itemCollapse1' aria-expanded='true' aria-controls='itemCollapse1'>"+
+              "<i class='fas fa-angle-double-down'></i></button></div>"
 
-            $("#notifytext").append($("<p class='text-gray wordbreak-keep-all'>" + result[0].reserv_num + "</p>"));
+              if (result[i].qna_answer != '') {
+                  review += "<div id='itemCollapse1' class='collapse show' aria-labelledby='itemHeading1' data-parent='#qnaItem1'>"+
+                          "<div class='card-body'><span class='state-text'>답변</span><p class='my-2 answer'>" + result[i].qna_answer +
+                          "</p><span class='state-text bottom'>"+ result[i].qna_answer_date + "</span></div></div>"
+              }
+
+              $("#lists").append($(
+                review + "</div></div>"
+              ))
+            }
           }
         }
       };
   
-      xhr.open("POST", "http://3.34.150.116:3000/reserv/user");
+      xhr.open("POST", "http://3.34.150.116:3000/studio/qna/user");
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-      xhr.send("key=" + id);
+      xhr.send("id=" + id);
     } else {
       alert("로그인이 필요합니다");
     }
-
-
 });
+
